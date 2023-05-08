ca079789ec0b6eb4b96a33e642f11fa486a503a4
ZanderCowboy <zanderkotze99@gmail.com>
2023-05-08 15:50:32 +0200

    Doing the assignment

--- OrdinaryDifferentialEquations/streamplot.py
+++ OrdinaryDifferentialEquations/streamplot.py
@@ -11,22 +11,28 @@ import numpy as np
 # plt.style.use('_mpl-gallery-nogrid')
 
 # make a stream function:
-xy_axis = 5
+xy_axis = 1.5
 x_axis_size = xy_axis
 y_axis_size = xy_axis
 
-X, Y = np.meshgrid(np.linspace(-x_axis_size, x_axis_size, 50), np.linspace(-y_axis_size, y_axis_size, 50))
-# U is x-derivative and V is y-derivative
-U = Y * (13 - X**2 - Y**2)
-V = 12 - X * (13 - X**2 - Y**2)
 
+x, y = np.meshgrid(np.linspace(-x_axis_size, x_axis_size, 50), np.linspace(-y_axis_size, y_axis_size, 50))
+# U is x-derivative and V is y-derivative
+# U = Y * (13 - X**2 - Y**2)
+u = y
+# V = 12 - X * (13 - X**2 - Y**2)
+v = x**4 - x**2
 
 
 # plot:
 fig, ax = plt.subplots()
+plt.xlim([-xy_axis, xy_axis])
+plt.ylim([-xy_axis, xy_axis])
+
+# start_points = np.array([[0, 2], [0, -2], [-2, 0], [2, 0]])
 
 # ax.streamplot(X[1:, 1:], Y[1:, 1:], U, V)
-ax.streamplot(X, Y, U, V)
+ax.streamplot(x, y, u, v, density=1.75)
 # ax.set(xlabel="x label", ylabel="y label")
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
---
+++ OrdinaryDifferentialEquations/test_ode.py
@@ -0,0 +1,60 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# # Define the range of x and v values
+# x = np.linspace(-2, 2, 100)
+# v = np.linspace(-2, 2, 100)
+#
+# # Create a meshgrid for x and v
+# X, V = np.meshgrid(x, v)
+#
+# # Calculate the potential energy for each (x, v) pair
+# # U = 0.5 * X**2 - 0.25 * X**4
+# U = (1/5) * X**5 - (1/3) * X**3
+#
+# # Calculate the time derivatives dx/dt and dv/dt
+# dX_dt = V
+# dV_dt = X**4 - X**2
+#
+# # Normalize the time derivatives for plotting
+# norm = np.sqrt(dX_dt**2 + dV_dt**2)
+# dX_dt_normalized = dX_dt / norm
+# dV_dt_normalized = dV_dt / norm
+#
+# # Plot the potential energy and the normalized time derivatives
+# plt.figure(figsize=(8, 6))
+# plt.contourf(X, V, U, levels=30, cmap='inferno')
+# plt.quiver(X, V, dX_dt_normalized, dV_dt_normalized, color='white')
+# plt.xlabel('x')
+# plt.ylabel('dx/dt (v)')
+# plt.title('Phase Diagram: Potential Energy and Velocity')
+# plt.grid(True)
+# plt.colorbar(label='Potential Energy')
+# plt.show()
+#
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the range of x and v values
+x = np.linspace(-5, 5, 150)
+v = np.linspace(-5, 5, 150)
+
+# Create a meshgrid for x and v
+X, V = np.meshgrid(x, v)
+
+# Define the parameters g and ω
+g = 9.8  # Acceleration due to gravity
+ω = 1.0  # Angular velocity
+
+# Calculate the time derivatives ˙x and ˙v
+dX_dt = V
+dV_dt = -g * X + (ω**2 - V**2) * X / (1 + X**2)
+
+# Plot the phase plane
+plt.figure(figsize=(8, 6))
+plt.quiver(X, V, dX_dt, dV_dt)
+plt.xlabel('x')
+plt.ylabel('˙x (v)')
+plt.title('Phase Plane')
+plt.grid(True)
+plt.show()
